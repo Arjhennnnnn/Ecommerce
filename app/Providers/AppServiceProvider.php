6a00e3e64d4c524a1575e7cab5b7369313556bd6
Arjhennnnnn <madlangsakay.arjhen05@gmail.com>
2024-05-16 11:22:20 +0800

    feat : Customize Response in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      * 
      **/
 
-     
+
     /**
      * Register any application services.
      */
@@ -40,13 +40,66 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Response::macro("success", function ($data, $message, $statusCode) {
+
+        /**
+         * Define a custom macro for generating success responses.
+         * 
+         * @param mixed $data => The data to be included in the response.
+         * 
+         * @param string $message => The message to be included in the response.
+         * 
+         * @param int $statusCode => The HTTP status code of the response.
+         * 
+         * @param string|null $resource => The resource class to be used for data transformation, if applicable.
+         * 
+         * @param bool $isMultipleData => Indicates whether the data consists of multiple items.
+         * 
+         * @return \Illuminate\Http\JsonResponse The success response.
+         * 
+         */
+
+        Response::macro("success", function ($data, $message, $statusCode, $resource = null, $isMultipleData = false,) {
+
+            function hasResource($resource)
+            {
+                return $resource != null;
+            }
+
+            if (hasResource($resource)) {
+
+                if ($isMultipleData) {
+
+                    $data = $resource::collection($data);
+                } else {
+
+                    $data = new $resource($data);
+                }
+            }
+
             return response()->json([
                 'success' => true,
                 'message' => $message,
                 'data' => $data
             ], $statusCode);
         });
+
+
+        /**
+         * Define a custom macro for generating error responses.
+         * 
+         * This macro constructs a JSON response indicating failure,
+         * 
+         * including an error message and optional data, with the provided HTTP status code.
+         * 
+         * @param mixed $data => The additional data to be included in the response.
+         * 
+         * @param string $errorMessage => The message describing the error.
+         * 
+         * @param int $statusCode => The HTTP status code to be returned in the response.
+         * 
+         * @return \Illuminate\Http\JsonResponse
+         */
+
         Response::macro("failed", function ($data, $errorMessage, $statusCode) {
             return response()->json([
                 'success' => false,
@@ -54,6 +107,21 @@ class AppServiceProvider extends ServiceProvider
                 'data' => $data
             ], $statusCode);
         });
+
+
+        /**
+         * Define a custom macro for generating error responses.
+         * 
+         * This macro constructs a JSON response indicating failure,
+         * 
+         * including an error message and optional data, with the provided HTTP status code.
+         * 
+         * @param string $exceptionMessage The message describing the error.
+         * 
+         * @param int $statusCode The HTTP status code to be returned in the response.
+         * 
+         * @return \Illuminate\Http\JsonResponse
+         */
 
         Response::macro("customException", function ($exceptionMessage, $statusCode) {
             return response()->json([
